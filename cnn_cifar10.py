10bf27c211afa42e7a3601bca4c680f521712a27
csusza <gykovacs93@gmail.com>
2019-04-06 00:15:35 +0200

    cnn_cifar10

---
+++ cnn_cifar10.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import keras
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import os
+from numpy.random import seed
+from tensorflow import set_random_seed
+
+seed(1)
+set_random_seed(1)
+learning_rate = 0.01
+batch_size = 32
+num_classes = 10
+epochs = 50
+num_predicts = 20
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = 'keras_cifar10_trained_model.h5'
+
+# The data, split between train and test sets:
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(filters=32, kernel_size=(3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.2))
+
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.2))
+
+model.add(Flatten())
+model.add(Dense(units=512))
+model.add(Activation('relu'))
+model.add(Dropout(rate=0.4))
+model.add(Dense(units=num_classes, activation='softmax'))
+
+# Initiate SGD optimizer
+opt = keras.optimizers.SGD(lr=learning_rate, momentum=0.0, decay=0.0, nesterov=False)
+
+# Training of the model
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+x_train = x_train.astype('float16')
+x_test = x_test.astype('float16')
+x_train /= 255
+x_test /= 255
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+
+# Save model and weights
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir, model_name)
+model.save(model_path)
+print('Saved trained model at %s ' % model_path)
+
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
