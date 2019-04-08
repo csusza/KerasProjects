9aabd9f98b4b7b86e06ac7904172ae909997a392
csusza <gykovacs93@gmail.com>
2019-04-08 16:37:18 +0200

    Now properly works with grayscale images

--- cnn_cats_and_dogs.py
+++ cnn_cats_and_dogs.py
@@ -4,23 +4,11 @@ import cv2
 from pathlib import Path
 import keras
 from keras.models import Sequential
-from keras.layers import Input, Dropout, Flatten, Conv2D, MaxPooling2D, Dense, Activation
-from keras.optimizers import RMSprop
-from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
+from keras.layers import Dropout, Flatten, Conv2D, MaxPooling2D, Dense, Activation
+from keras.callbacks import Callback, EarlyStopping
 from keras import backend as K
 import matplotlib.pyplot as plt
-from keras.utils import np_utils
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 K.set_image_data_format('channels_first')
-
-# datagen = ImageDataGenerator(
-#         rotation_range=40,
-#         width_shift_range=0.2,
-#         height_shift_range=0.2,
-#         shear_range=0.2,
-#         zoom_range=0.2,
-#         horizontal_flip=True,
-#         fill_mode='nearest')
 
 
 TRAIN_PATH = Path.cwd() / 'cad_pictures' / 'train'
@@ -28,27 +16,31 @@ TEST_PATH = Path.cwd() / 'cad_pictures' / 'test'
 
 
 ROWS = COLS = 128
-CHANNELS = 3
-learning_rate = 0.001
+CHANNELS = 1
+learning_rate = 0.0003
 nb_epoch = 100
 batch_size = 32
 
-
+# Read input path
 train_images = np.array([TRAIN_PATH / i for i in os.listdir(TRAIN_PATH)])
 test_images = np.array([TEST_PATH / i for i in os.listdir(TEST_PATH)])
 train_cats = np.array([TRAIN_PATH / i for i in os.listdir(TRAIN_PATH) if 'cat' in i])
 train_dogs = np.array([TRAIN_PATH / i for i in os.listdir(TRAIN_PATH) if 'dog' in i])
 
+# Subsample the inputs
 train_images = np.append(train_cats[:5000], train_dogs[:5000])
 np.random.shuffle(train_images)
-test_images = test_images[:100]
+test_images = test_images[:1000]
 print(train_images.shape)
 print(test_images.shape)
 
 
 def read_image(file_path):
-        img = cv2.imread(str(file_path), cv2.IMREAD_COLOR)  # cv2.IMREAD_GRAYSCALE
-        return cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_LANCZOS4)
+    if CHANNELS == 3:
+        img = cv2.imread(str(file_path), cv2.IMREAD_COLOR)
+    else:
+        img = cv2.imread(str(file_path), cv2.IMREAD_GRAYSCALE)
+    return cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_LANCZOS4)
 
 
 def prep_data(images):
@@ -63,10 +55,11 @@ def prep_data(images):
         return data
 
 
+# Read images, split between train and test sets, normalize the data
 train = prep_data(train_images)
 test = prep_data(test_images)
-train = train.astype('float32')
-test = test.astype('float32')
+train = train.astype('float16')
+test = test.astype('float16')
 train /= 255
 test /= 255
 
@@ -82,33 +75,34 @@ for i in range(train_images.shape[0]):
         labels.append(0)
 
 
+# Setting up the network
 def catdog():
     model = Sequential()
 
     model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', input_shape=(CHANNELS, ROWS, COLS), activation='relu',))
     model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu',))
-    model.add(MaxPooling2D(pool_size=(3, 3),))
-
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu',))
-    model.add(Conv2D(filters=32, kernel_size=(3, 3), padding='same', activation='relu',))
-    model.add(MaxPooling2D(pool_size=(3, 3),))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2),))
 
     model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu',))
     model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu',))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2),))
+
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu',))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', activation='relu',))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2),))
+
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu',))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu',))
     model.add(MaxPooling2D(pool_size=(3, 3),))
 
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu',))
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu',))
-    model.add(MaxPooling2D(pool_size=(3, 3),))
-
-    # model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu'))
-    # model.add(Conv2D(filters=256, kernel_size=(3, 3), padding='same', activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(3, 3)))
+    # model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu',))
+    # model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu',))
+    # model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2),))
 
     model.add(Flatten())
-    model.add(Dense(units=512, activation='relu',))
+    model.add(Dense(units=256, activation='relu',))
     model.add(Dropout(0.3))
-    model.add(Dense(units=512, activation='relu',))
+    model.add(Dense(units=256, activation='relu',))
     model.add(Dropout(0.3))
 
     model.add(Dense(1))
@@ -116,6 +110,7 @@ def catdog():
 
     opt = keras.optimizers.SGD(lr=learning_rate, momentum=0.0, decay=0.0001, nesterov=False)
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    print(model.summary())
     return model
 
 
@@ -133,7 +128,7 @@ class LossHistory(Callback):
         self.val_losses.append(logs.get('val_loss'))
 
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=6, verbose=1, mode='auto')
+early_stopping = EarlyStopping(monitor='val_loss', patience=16, verbose=1, mode='auto')
 
 def run_catdog():
     history = LossHistory()
@@ -152,21 +147,24 @@ predictions = model.predict(test, verbose=0)
 
 loss = history.losses
 val_loss = history.val_losses
+cv2.waitKey(0)
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.title('VGG-16 Loss Trend')
 plt.plot(loss, 'blue', label='Training Loss')
 plt.plot(val_loss, 'green', label='Validation Loss')
-plt.xticks(range(0,nb_epoch)[0::2])
+plt.xticks(range(0, nb_epoch)[0::2])
 plt.legend()
 plt.show()
 
-for i in range(0, 10):
+for i in range(0, 3):
     if predictions[i, 0] >= 0.5:
         print('I am {:.2%} sure this is a Dog'.format(predictions[i][0]))
     else:
         print('I am {:.2%} sure this is a Cat'.format(1 - predictions[i][0]))
-
-    plt.imshow(np.array(test[i].T))
+    if CHANNELS == 3:
+        plt.imshow(np.array(test[i].T.astype('float32')))
+    else:
+        plt.imshow(np.reshape(np.array(test[i].T.astype('float32')), (ROWS, COLS)), cmap='gray')
     plt.show()
 
